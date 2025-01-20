641a8b8a51012fcf3e31b903d07b5f1e086505fd
kaii <newbyasal@gmail.com>
2025-01-20 13:02:48 +0330

    solve calculateAverage

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -10,4 +10,12 @@
  * calculateAverage([10, 20, 30]) should return 20.
  */
 
+function calculateAverage(list){
+    if (list.length==0) return NaN;
+    var sum=list.reduce((previousval,currentval)=>{
+        return previousval+currentval;
+    });
+    return sum/list.length;
+}
+
 module.exports = calculateAverage;
