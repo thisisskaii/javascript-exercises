d6f452395419c46bc4ee29243eae8d273d64d269
kaii <newbyasal@gmail.com>
2025-01-20 22:56:20 +0330

    solve numberToWords

--- advanced/numberToWords.js
+++ advanced/numberToWords.js
@@ -11,5 +11,44 @@
  */
 
 // Write your solution here
+function numberToWords(num){
+    let nums1_9=['','one','two','three','four','five','six','seven','eight','nine'];
+    let nums11_19=['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+    let nums10s=['','ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let bigNums = ['','thousand', 'million']
+    function Hundreds(num) {
+        let result = '';
+        if (num > 99) {
+            result += nums1_9[Math.floor(num / 100)] + ' hundred ';
+            num %= 100;
+        }
+        if (num > 10 && num < 20) {
+            result += nums11_19[num - 11] + ' ';
+        } else {
+            if (num > 9) {
+                result += nums10s[Math.floor(num / 10)] + ' ';
+                num %= 10;
+            }
+            result += nums1_9[num] + ' ';
+        }
+        return result.trim();
+    }
+
+    if (num === 0) return 'zero';
+
+    let word = '';
+    let bigNumsIndex = 0;
+
+    while (num > 0) {
+        let digitGroup = num % 1000;
+        if (digitGroup) {
+            word = Hundreds(digitGroup) + ' ' + bigNums[bigNumsIndex] + ' ' + word;
+        }
+        bigNumsIndex++;
+        num = Math.floor(num / 1000);
+    }
+
+    return word.trim();
+}
 
 module.exports = numberToWords;
