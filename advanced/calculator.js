28f147ba5958e8f09b92bd20eac2a0aedd87d445
kaii <newbyasal@gmail.com>
2025-01-20 12:52:36 +0330

    solve calculator

--- advanced/calculator.js
+++ advanced/calculator.js
@@ -15,4 +15,18 @@
 
 // Write your solution here
 
+function add(num1,num2){
+    return num1+num2;
+}
+function subtract(num1,num2){
+    return num1-num2;
+}
+function multiply(num1,num2){
+    return num1*num2;
+}
+function divide(num1,num2){
+    if(num2==0) return NaN
+    return num1/num2
+}
+
 module.exports = { add, subtract, multiply, divide };
